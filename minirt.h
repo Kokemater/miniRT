7dac2aea3532c81542b12ff051ecb2d8f2ef0431
David Moraleda Salguero <dmoraled@c1r12s4.42madrid.com>
2025-08-14 12:04:47 +0200

    .h

--- minirt.h
+++ minirt.h
@@ -2,10 +2,32 @@
 # define MINIRT_H
 
 # include <stdio.h>
+# include <unistd.h>
 # include <fcntl.h>
 
 # include "libs/minilibx-linux/mlx.h"
 # include "libs/libft/libft.h"
+
+typedef struct s_state
+{
+    void    *mlx;
+    void    *win;
+    int     counter;
+}   t_state;
+
+typedef struct s_vec3
+{
+	float	x;
+	float	y;
+	float	z;
+}	t_vec3;
+
+typedef struct s_color
+{
+	char	r;
+	char	g;
+	char	b;
+}	t_color;
 
 
 #endif 
